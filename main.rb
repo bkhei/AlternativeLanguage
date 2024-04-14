145dea0573c04b1e1578485a51c7b9df9dc07ef6
Marcus Watson <47387636+bkhei@users.noreply.github.com>
2024-04-13 21:40:33 -0400

    Updating for repo

--- main.rb
+++ main.rb
@@ -1,3 +1,8 @@
+# For this project, you will read in a CSV file Download CSV file that has statistics for 1000 cell phones.
+  # You will then create a class called Cell and assign the column as class attributes. Create getter and setter methods.
+  # The objects will be stored in a data structure native to the language.
+  # You are free to pick what your data structure is but if your language has a HashMap type data structure, I strongly encourage you using it.
+
 # Import built-in CSV library
 require 'csv'
 
@@ -12,7 +17,6 @@ end
 
 # Create a class named "Cell"
 class Cell
-  
   # Class variable
   @@column = nil
 
@@ -21,13 +25,18 @@ class Cell
     @@column = value
   end
 
-  # Getter method 
-    def self.column
-      @@column
-    end
+  # Getter method
+  def self.column
+    @@column
+  end
 end
 
-# Set class attributes 
-Cell.column = "A"
-puts Cell.column
-puts 
+
+
+# Data Ingestion and Cleaning
+  # You must perform transformations on the data as it comes in or after it is been ingested.
+  # This a process called data cleaning and these are the steps:
+    # Replace all missing or "-" values with null or something similar that can be ignored during calculations.
+    # Transform data in appropriate columns according to instructions.
+    # For example in the body_weight column, a typical value is '190 g (6.70 oz)'' and needs to be converted to 190.
+    # Convert data types in appropriate columns
