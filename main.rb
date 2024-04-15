9d28a239f3158900b72a46bc39a6ca3e18c30e8d
Marcus Watson <47387636+bkhei@users.noreply.github.com>
2024-04-15 15:44:11 +0000

    Function to write to CSV for testing + debugging, adding Gems

--- main.rb
+++ main.rb
@@ -1,77 +1,50 @@
 =begin
-Methods
-  Within each class, you will create a series of methods that will perform operations on the new objects.
-  There must be at least 7 methods/functions in your Cell class, not including getter and setter methods or the methods you use for transformation.
-  Be creative and think critically, its your choice on what these are.
-  Some ideas:
-    - ToString method that will convert your objects details to a string for printing.
-    - Calculate statistics on columns, for numeric, descriptive stats such as mean, median, standard deviation, etc. For categorical columns, perhaps Mode or count of unique values.
-    Listing of unique values for each column.
-    Ability to add an object and input data for each object's variable.
-    Ability to delete an object.
-
-Exceptions
-   Ensure that you have exceptions that can handle errors.
-    Focus on input validation. You do not need to validate everything,
-    but have a robust number of exception handling in your project to show you are making your program secure and able to survive poor user input.
-
-Testing
- If your programming language has unit testing capability such as junit or pyunit, use that for your unit tests. If not, write your own unit tests for each method. On top of a unit test for each method, there are three tests you are required to have:
-  Ensure the file being read is not empty.
-  Ensure each column's final transformation matches what is stated above as its final form (ex: test if display_size is now a float)
-  Ensure all missing or "-" data is replaced with a null value.
-
- # Data Ingestion and Cleaning
-  # You must perform transformations on the data as it comes in or after it is been ingested.
-    # 1) Replace all missing or "-" values with null or something similar that can be ignored during calculations.
-    # 2) Transform data in appropriate columns according to instructions (ex: body_weight column, '190 g (6.70 oz)'' > 190)
-    # 3) Convert data types in appropriate columns
-
-    oem is treated as a string // Replace missing or "-" values with null
-    model is treated as a string // Replace missing or "-" values with null
-    launch_announced is treated as integer (Year) // Values containing "V1" must be replaced with null
-    launch_status is treated as a string (Year or "Discontinued" or "Cancelled") //
-    body_dimensions is treated as a string (Height x Width x Thickness in mm or inches) // Replace missing or "-" values with null
-    body_weight is treated as an float (in grams) // String w/ integer in it before letter g
-    body_sim is treated as a string // Replace "No" with null, replace "Yes" w/ "Mini-SIM"
-    display_type is treated as a string // Replace missing or "-" values with null
-    display_size is treated as a float (in inches) // String w/ integer or float before the word "inches"
-    display_resolution is treated as a string // Replace missing or "-" values with null
-    features_sensors is treated as a string // Replace missing or "-" values with null. Replace integers w/ null
-    platform_os is treated as a string // Replace instances of floats to integers (e.g. "Android 4.4.2" to "Android 4")
-
+#     oem is treated as a string // Replace missing or "-" values with null
+#     model is treated as a string // Replace missing or "-" values with null
+#     launch_announced is treated as integer (Year) // Values containing "V1" must be replaced with null
+#     launch_status is treated as a string (Year or "Discontinued" or "Cancelled") //
+#     body_dimensions is treated as a string (Height x Width x Thickness in mm or inches) // Replace missing or "-" values with null
+#     body_weight is treated as an float (in grams) // String w/ integer in it before letter g
+#     body_sim is treated as a string // Replace "No" with null, replace "Yes" w/ "Mini-SIM"
+#     display_type is treated as a string // Replace missing or "-" values with null
+#     display_size is treated as a float (in inches) // String w/ integer or float before the word "inches"
+#     display_resolution is treated as a string // Replace missing or "-" values with null
+#     features_sensors is treated as a string // Replace missing or "-" values with null. Replace integers w/ null
+#     platform_os is treated as a string // Replace instances of floats to integers (e.g. "Android 4.4.2" to "Android 4")
 =end
+
 # For this project, you will read in a CSV file Download CSV file that has statistics for 1000 cell phones.
+
 # Import built-in CSV library
+# Import library to write to an excel file
 require 'csv'
-require 'descriptive_statistics'
+require 'write_xlsx'
 
 # Read CSV file
 cells = CSV.read('cells.csv', headers: true)
 
 # Iterate over the array of arrays, going through each element for the rows of the CSV.
 # 'inspect' method returns a string representation of the object it's called on.
-
 cells.each do |row|
   puts row.inspect
 end
 
-# You will then create a class called Cell and assign the column as class attributes. Create getter and setter methods.
 # Creating a class named "Cell", and assigning each column as class attributes
+# attr_accessor functions as both a getter and setter method in Ruby
 class Cell
   attr_accessor :oem,
-  :model,
-  :launch_announced,
-  :launch_status,
-  :body_dimensions,
-  :body_weight,
-  :body_sim,
-  :display_type,
-  :display_size,
-  :display_resolution,
-  :features_sensors,
-  :platform_os
-
+                :model,
+                :launch_announced,
+                :launch_status,
+                :body_dimensions,
+                :body_weight,
+                :body_sim,
+                :display_type,
+                :display_size,
+                :display_resolution,
+                :features_sensors,
+                :platform_os
+# Function containing our methods
   def initialize(attributes = {})
     # Replace missing or "-" values with null
     @oem = attributes['oem'].empty? || attributes['oem'] == '-' ? nil : attributes['oem']
@@ -85,8 +58,8 @@ class Cell
       @launch_announced = nil
     # Check to see if launch_announced contains a year, then capture the first year encountered and store it as an integer
     else
-      year = launch_anhnounced && launch_announced.scan(/\d{4}/).first
-    @launch_announced = year ? year.to_i : nil
+      year = launch_announced && launch_announced.scan(/\d{4}/).first
+      @launch_announced = year ? year.to_i : nil
     end
 
     # Extract year from launch_status
@@ -107,29 +80,29 @@ class Cell
     # Extract the body weight from the string and convert to a float
     body_weight = attributes['body_weight']
     # Check to encounter the first integer or float in the string and store it as a float
-    weight_in_grams = body_weight && body_weight.scan(/\d+(\.\d+)?/).first
+    weight_in_grams = nil if
+      weight_in_grams.is_a?(Array) && weight_in_grams.first.nil?
     @body_weight = weight_in_grams ? weight_in_grams.to_f : nil
 
     # Extract the SIM card type from the string
     body_sim = attributes['body_sim']
     # Check to see if body_sim contains oddities (Yes/No)
-    if body_sim == 'No' || body_sim == 'Yes'
-      @body_sim = nil
-    else
-      @body_sim = body_sim
-    end
+    @body_sim = if %w[No Yes].include?(body_sim)
+                  nil
+                else
+                  body_sim
+                end
 
     # Replace missing or "-" values with null
     @display_type = attributes['display_type'].empty? || attributes['display_type'] == '-' ? nil : attributes['display_type']
 
-
     # Extract the display size from the string and convert to a float
     display_size = attributes['display_size']
-    size_in_inches = display_size && display_size.scan(/\d+(\.\d+)?/).first
+    size_in_inches = display_size && display_size.scan(/(\d+(\.\d+)?)/).flatten.first
     @display_size = size_in_inches ? "#{size_in_inches} inches" : nil
 
     # Replace missing or "-" values with null
-    @display_resolution = attributes['display_resolution'].empty? || attributes['display_resolution'] == '-' ? nil : attributes['display_resolution'] # Replace missing or "-" values with null
+    @display_resolution = attributes['display_resolution'].nil? || attributes['display_resolution'] == '-' ? nil : attributes['display_resolution'] 
 
     # Replace missing or "-" values with null
     # For unit testing purposes, remember the Cells.csv doesn't have invalid attributes in this column
@@ -139,10 +112,9 @@ class Cell
     platform_os = attributes['platform_os']
     first_os = platform_os && platform_os.split(',', 2).first
     @platform_os = first_os
-
   end
 
-  # Method that will convert objects details to a string for printing
+  # Method that will convert object details to a string for printing
   def to_s
     body_weight_str = @body_weight && @body_weight % 1 == 0 ? @body_weight.to_i.to_s : @body_weight.to_s
     "OEM: #{@oem},
@@ -165,8 +137,6 @@ class Cell
   end
 end
 
-# The objects will be stored in a data structure native to the language.
-# You are free to pick what your data structure is but if your language has a HashMap type data structure, I strongly encourage you using it.
 # Creating a data structure to store each phone in hash
 phones = {}
 
@@ -176,6 +146,15 @@ cells.each do |row|
   phones[cell.model] = cell
 end
 
-phones.each do |model, cell |
-  puts cell.to_s
+# Open a new CSV file
+CSV.open("transformed_data.csv", "wb") do |csv|
+  # Write the headers to the CSV file
+  headers = ['OEM', 'Model', 'Launch Announced', 'Launch Status', 'Body Dimensions', 'Body Weight', 'Body SIM', 'Display Type', 'Display Size', 'Display Resolution', 'Features Sensors', 'Platform OS']
+  csv << headers
+
+  # Write the data to the CSV file
+  phones.each do |model, cell|
+    data = [cell.oem, model, cell.launch_announced, cell.launch_status, cell.body_dimensions, cell.body_weight, cell.body_sim, cell.display_type, cell.display_size, cell.display_resolution, cell.features_sensors, cell.platform_os]
+    csv << data
+  end
 end
